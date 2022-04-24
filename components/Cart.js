5e51157211e30e8a733ab156006d2be4965ee54f
Your Name <you@example.com>
2022-04-24 15:09:44 +0300

    twoo

--- components/Cart.js
+++ components/Cart.js
@@ -8,7 +8,7 @@ import toast from 'react-hot-toast';
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
 import getStripe from '../lib/getStripe';
-export default function cart() {
+export default function Cart() {
 
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
